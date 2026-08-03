42b2c91caa73a1f9cbf75583ee80479f30b47e68
Stogalosu <dumisteck@gmail.com>
2026-08-03 19:51:42 +0300

    Added login form. Changed color scheme.

---
+++ src/app/login/page.tsx
@@ -0,0 +1,39 @@
+import styles from "./page.module.css";
+import NavBar from "@/components/NavBar/NavBar";
+import Spacer from "@/components/Spacer";
+import { Mail, KeyRound } from "lucide-react";
+import Form from "next/form";
+
+export default function Login() {
+    return(
+        <>
+            <NavBar selectedPage="/login"></NavBar>
+            <main className={styles.main}>
+                <div className={styles.card}>
+                    <div className={styles.title}>Login</div>
+                    <Spacer height={30} width={1}></Spacer>
+                    <Form id="login-form" action="">
+                        <div className={styles.loginForm}>
+                            <div className={styles.loginField}>
+                                <Mail className={styles.icon}/>
+                                <input
+                                    name="email"
+                                    placeholder="Email"
+                                    className={styles.searchInput}
+                                />
+                            </div>
+                            <div className={styles.loginField}>
+                                <KeyRound className={styles.icon}/>
+                                <input
+                                    name="password"
+                                    placeholder="Password"
+                                    className={styles.searchInput}
+                                />
+                            </div>
+                        </div>
+                    </Form>
+                </div>
+            </main>
+        </>
+    )
+}
