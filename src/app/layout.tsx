63516e31315ff81c75fe38db10caa515fa25603f
Stogalosu <dumisteck@gmail.com>
2026-08-04 00:09:22 +0300

    Added hover behaviour for buttons.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import styles from "./page.module.css";
+import { Toaster } from "sonner";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -26,6 +27,7 @@ export default function RootLayout({
   return (
     <html lang="en" className={`${geistSans.variable} ${geistMono.variable}`}>
       <body>
+        <Toaster/>
         <div className={styles.container}>
           {children}
         </div>
