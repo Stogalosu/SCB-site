0bd0f50a440131d80c452d98e5e01384d8522a0c
Stogalosu <dumisteck@gmail.com>
2026-08-03 14:35:19 +0300

    Initial commit. Configured PostgreSQL database.

---
+++ src/lib/auth.ts
@@ -0,0 +1,22 @@
+import { betterAuth } from "better-auth";
+import { prismaAdapter } from "better-auth/adapters/prisma";
+import prisma from "@/lib/prisma";
+
+export const auth = betterAuth({
+    database: prismaAdapter(prisma, {
+        provider: "postgresql",
+    }),
+    emailAndPassword: {
+        enabled: true,
+    },
+    advanced: {
+        database: {
+            generateId: (options) => {
+                if (options.model === "user") {
+                    return false;
+                }
+                return crypto.randomUUID();
+            },
+        },
+    },
+});
