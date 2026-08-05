530ccd83e4faedcda50cbd80f0d9110966429564
Stogalosu <dumisteck@gmail.com>
2026-08-05 17:36:48 +0300

    Separated layout CSS.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-import styles from "./page.module.css";
+import styles from "./layout.module.css";
 import { Toaster } from "sonner";
 
 const geistSans = Geist({
