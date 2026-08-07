c2ab7456920148fcf0b916899786c76f62ffc9d9
Stogalosu <dumisteck@gmail.com>
2026-08-07 22:52:59 +0300

    Updated primary button to also accept links as props.

--- src/components/PrimaryButton/PrimaryButton.tsx
+++ src/components/PrimaryButton/PrimaryButton.tsx
@@ -3,10 +3,17 @@
 import styles from "./PrimaryButton.module.css";
 import React from "react";
 
-export default function PrimaryButton({ children, onClick }: { children: React.ReactNode, onClick: () => void }) {
-    return (
-        <button className={styles.primaryButton} onClick={onClick}>
-            {children}
-        </button>
-    )
+export default function PrimaryButton({ children, link, onClick }: { children: React.ReactNode, link?: string, onClick?: () => void }) {
+    if(onClick)
+        return (
+            <button className={styles.primaryButton} onClick={onClick}>
+                {children}
+            </button>
+        );
+    if(link)
+        return (
+            <a className={styles.primaryButton} href={link}>
+                {children}
+            </a>
+        );
 }
